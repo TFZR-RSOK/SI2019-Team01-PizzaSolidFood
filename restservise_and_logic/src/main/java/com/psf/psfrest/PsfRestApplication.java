14569fdccfafaf1a867249a8fb5cc9c64a74547a
Kristian Kalamin <kalamin.michal@gmail.com>
2019-03-24 19:35:59 +0100

    Kreiran REST projekat

---
+++ restservise_and_logic/src/main/java/com/psf/psfrest/PsfRestApplication.java
@@ -0,0 +1,13 @@
+package com.psf.psfrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsfRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsfRestApplication.class, args);
+	}
+
+}
