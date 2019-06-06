03d4e9dc4b2d8734f489d3f12f2da33c9e2f0d0d
Nikola Turudija <turudija007@gmail.com>
2019-06-06 21:16:08 +0200

    Sign Up page added

--- frontend-php/PizzaSolidFood/resources/views/home.blade.php
+++ frontend-php/PizzaSolidFood/resources/views/home.blade.php
@@ -26,6 +26,7 @@
                     <li><a href="#gallery-content">Gallery</a></li>
                     <li><a href="#our-story">About Us</a></li>
                     <li><a href="#contact">Contact</a></li>
+                    <li><a href="{{ url('/signup') }}" class="sign-up">Sign Up</a></li>
                 </ul>
                 <div class="menu-trigger"><i class="fas fa-bars"></i></div>
             </nav>
